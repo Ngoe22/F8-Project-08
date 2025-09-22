6b421f125fb3f449bb5873525b54c5344f95fc77
Ngoe22 <ngoeee0402@gmail.com>
2025-09-22 23:53:29 +0700

    1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,24 +23,11 @@ Copy;
  * <button class="js-toggle" toggle-target="#box">Click</button>
  * <div id="box">Content show/hide</div>
  */
-window.addEventListener("template-loaded", initJsToggle);
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("mb-navbar-btn");
+    const item = document.getElementById("mb-navbar");
 
-function initJsToggle() {
-    $$(".js-toggle").forEach((button) => {
-        const target = button.getAttribute("toggle-target");
-        if (!target) {
-            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
-        }
-        button.onclick = () => {
-            if (!$(target)) {
-                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
-            }
-            const isHidden = $(target).classList.contains("hide");
-
-            requestAnimationFrame(() => {
-                $(target).classList.toggle("hide", !isHidden);
-                $(target).classList.toggle("show", isHidden);
-            });
-        };
+    btn.addEventListener("click", () => {
+        item.classList.toggle("active");
     });
-}
+});
